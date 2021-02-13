a3309f9a9e75a83f8c04875da6b4cb9c8d372a27
Roman Lukash <roman.lukash@groupm.tech>
2021-02-13 15:56:23 +0200

    WIP

--- src/test/groovy/com/sparkTutorial/rdd/airports/AirportsInUsaProblemTest.groovy
+++ src/test/groovy/com/sparkTutorial/rdd/airports/AirportsInUsaProblemTest.groovy
@@ -7,21 +7,17 @@ class AirportsInUsaProblemTest extends Specification {
     def "happy path"() {
 
         when:
-        def testClass = new AirportsInUsaProblem()
-        testClass.main(new String[]{})
+        new AirportsInUsaProblem().main(new String[]{})
 
         then:
-        def airports = [:]
         def file = new File(AirportsInUsaProblem.OUTPUT_FILE)
+        file.exists()
         println file.absolutePath
         def expectedCount = 1697
         def count = 0
 
         def l=file.eachLine { line,i ->
-            if(i==1)return
-            def fields=line.split(",")
-            airports.put(fields[0], fields[1])
-            count++
+            if(i>1)count++
         }
         expectedCount == count
 
